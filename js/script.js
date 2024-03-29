704211ed270ba9d19c31490440758f37e47c7075
Sinkazol <81185967+Sinkazol@users.noreply.github.com>
2024-03-29 19:57:50 +0100

    code formating

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-function showSidebar(){
-    const sidebar =document.querySelector('.sidebar')
-    sidebar.style.display='flex';
-    
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex';
+
 }
-function hideSidebar(){
-    const sidebar =document.querySelector('.sidebar')
-    sidebar.style.display='none';
-   
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none';
+
 }
 
 var darkmode = document.getElementById("darkModeSwitch");
 
-darkmode.onclick=function(){
+darkmode.onclick = function () {
     document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        darkModeSwitch.src="../images/Icons/sun.svg" 
-    }else{
-        darkModeSwitch.src="../images/Icons/moon.svg";
+    if (document.body.classList.contains("dark-theme")) {
+        darkModeSwitch.src = "../images/Icons/sun.svg"
+    } else {
+        darkModeSwitch.src = "../images/Icons/moon.svg";
     }
 }
 
