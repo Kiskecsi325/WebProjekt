26de41b20ba288680497ff73859647d55ef3d49e
Sinkazol <81185967+Sinkazol@users.noreply.github.com>
2024-03-29 19:18:36 +0100

    darkmode 1

--- js/script.js
+++ js/script.js
@@ -9,43 +9,14 @@ function hideSidebar(){
    
 }
 
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Az alapértelmezett esemény megszakítása (az űrlap küldésének megakadályozása)
+var darkmode = document.getElementById("darkModeSwitch");
 
-    // Ellenőrizd a mezőket
-    let formIsValid = true;
-    document.querySelectorAll('.inputBox input, .inputBox textarea').forEach(function(input) {
-        if (!input.checkValidity()) {
-            formIsValid = false;
-            input.parentNode.querySelector('.error-txt').style.display = 'block'; // Hibaszöveg megjelenítése
-        } else {
-            input.parentNode.querySelector('.error-txt').style.display = 'none'; // Hibaszöveg elrejtése
-        }
-    });
-
-    // Ellenőrizd az email címet
-    const emailInput = document.querySelector('input[type="email"]');
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (emailInput && !emailPattern.test(emailInput.value)) {
-        formIsValid = false;
-        emailInput.parentNode.querySelector('.error-txt').style.display = 'block';
-    } else if (emailInput) {
-        emailInput.parentNode.querySelector('.error-txt').style.display = 'none';
+darkmode.onclick=function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        darkModeSwitch.src="../images/Icons/sun.svg" 
+    }else{
+        darkModeSwitch.src="../images/Icons/moon.svg";
     }
+}
 
-    // Ellenőrizd a telefonszámot
-    const phoneInput = document.querySelector('input[type="tel"]');
-    const phonePattern = /^\+(?:36)(?:20|30|70)\d{3}\d{2}\d{2}$/;
-    if (phoneInput && !phonePattern.test(phoneInput.value)) {
-        formIsValid = false;
-        phoneInput.parentNode.querySelector('.error-txt').style.display = 'block';
-    } else if (phoneInput) {
-        phoneInput.parentNode.querySelector('.error-txt').style.display = 'none';
-    }
-
-    // Ha az űrlap érvényes, elküldheted az adatokat
-    if (formIsValid) {
-        // Ide írd meg az adatküldés logikáját vagy hívj egy funkciót, ami ezt végzi
-        alert('Az űrlap érvényes, elküldhető!');
-    }
-});
